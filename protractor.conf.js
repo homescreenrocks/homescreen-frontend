1071a147de13c77fd1461f50464c66ac13b507d6
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-07-27 19:44:35 +0200

    updates dependencies

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,4 @@
+
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
@@ -19,12 +20,10 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  beforeLaunch: function() {
+  onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-  },
-  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
